372b112835d32c832a3f4db1f014e2c729c8ab24
kirill morozov <jobkir271@gmail.com>
2026-09-04 12:23:04 +0700

    fix broken links test: handle text in body or iframe

--- tests/elements_tests/test_broken.py
+++ tests/elements_tests/test_broken.py
@@ -28,10 +28,15 @@ def test_broken_links(test_broken):
     with allure.step("Open broken page"):
         page = test_broken
     with allure.step("Click valid link and check URL"):
-        page.get_by_role('link',name ="Click Here for Valid Link").click()
+        page.get_by_role('link', name="Click Here for Valid Link").click()
         expect(page).to_have_url("https://demoqa.com/")
-
-    page.go_back()
+        page.go_back()
     with allure.step("Click broken link and check error text"):
-        page.get_by_role('link',name ="Click Here for Broken Link").click()
-        expect(page.locator("body")).to_contain_text("500")
+        page.get_by_role('link', name="Click Here for Broken Link").click()
+        page.wait_for_load_state("networkidle")
+        body = page.locator("body")
+        try:
+            expect(body).to_contain_text("500", timeout=5000)
+        except AssertionError:
+            frame = page.frame_locator("iframe")
+            expect(frame.locator("body")).to_contain_text("500")
